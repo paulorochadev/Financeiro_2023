9c2442c6a54efca84f477a22625c324fdb69cf11
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-07 10:07:53 -0300

    chore: Cria Repositorio Generico e configura Banco e Identity

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Domain.Interfaces.Generics;
+using Entities.Entidades;
+using Infra.Configuracao;
+using Infra.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuração Banco de Dados
+builder.Services.AddDbContext<ContextBase>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ContextBase>();
+
+// Interface e Repositorio
+builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositoryGenerics<>));
 
 var app = builder.Build();
 
