154f705b251b83d99e44e0c9e4f0105cba344348
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-07 08:57:08 -0300

    chore: Configura String de Conexão para acesso ao Banco de Dados SqlServer

---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,44 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Despesa> Despesa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=LIONTECH-DELL\\SQLEXPRESS;Initial Catalog=Financeiro_2023;Integrated Security=True";
+        }
+    }
+}
