7ac22694176e1b93aa591b0d3777341e22a65d64
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-07 15:41:02 -0300

    chore: Implementa métodos nas classes dos Serviços

---
+++ Domain/Servicos/DespesaServico.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServicos;
+using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Servicos
+{
+    public class DespesaServico : IDespesaServico
+    {
+        private readonly InterfaceDespesa _interfaceDespesa;
+
+        public DespesaServico(InterfaceDespesa interfaceDespesa)
+        {
+            _interfaceDespesa = interfaceDespesa;
+        }
+
+        public async Task AdicionarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataCadastro = data;
+            despesa.Ano = data.Year;
+            despesa.Mes = data.Month;
+
+            var valido = despesa.ValidaPropriedadeString(despesa.Nome, "Nome");
+
+            if (valido)
+            {
+                await _interfaceDespesa.Add(despesa);
+            }
+        }
+
+        public async Task AtualizarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataAlteracao = data;
+
+            if (despesa.Pago)
+            {
+                despesa.DataPagamento = data;
+            }
+
+            var valido = despesa.ValidaPropriedadeString(despesa.Nome, "Nome");
+
+            if (valido)
+            {
+                await _interfaceDespesa.Update(despesa);
+            }
+        }
+    }
+}
