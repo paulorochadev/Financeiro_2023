6e89409f5a94c6416c5b76ae2b4c1b43f3e81dd7
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-09 10:07:17 -0300

    chore: Cria pasta e classes TokenJWT

---
+++ WebApi/Token/TokenJWT.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace WebApi.Token
+{
+    public class TokenJWT
+    {
+        private JwtSecurityToken _token;
+
+        internal TokenJWT(JwtSecurityToken token)
+        {
+            this._token = token;
+        }
+
+        public DateTime ValidTo => _token.ValidTo;
+
+        public string Value => new JwtSecurityTokenHandler().WriteToken(this._token);
+    }
+}
